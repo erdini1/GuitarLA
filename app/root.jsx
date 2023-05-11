c981c0fecac7ba3b398d330b2ff909e07ee36920
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-11 11:34:15 -0300

    Agregando la funcion meta al root

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,20 @@
+import {
+    Meta
+} from "@remix-run/react"
+
+//Esto es el doctype de HTML, asi se importa
+export function meta() {
+    return (
+        [
+            { charset: "utf-8" },
+            { title: "GuitarLA - Remix" },
+            { name: "viewport", content: "width=device-width,initial-scale=1" }
+        ]
+    )
+}
+
 // En primer lugar hay que exportar la función App
-export default function App(){
+export default function App() {
     return (
         <Document>
             <h1>Desde App</h1>
@@ -7,11 +22,11 @@ export default function App(){
     )
 }
 
-function Document({children}){
-    return(
+function Document({ children }) {
+    return (
         <html lang="es">
             <head>
-                <title>GuitarLA - Remix</title>
+                <Meta />
             </head>
             <body>
                 {children}
