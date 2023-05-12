928ef01f331e5e86a7fac6f9b1c09a38791c95dc
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-12 12:38:22 -0300

    Resaltando la pagina actual con useLocation

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,12 @@
-import { Link } from "@remix-run/react"
+import { Link, useLocation } from "@remix-run/react"
 import logo from "../../public/img/logo.svg"
 
 const Header = () => {
+
+    // Esto lo hacemos para resaltar en la pagina en que estamos dentro de header
+    const location = useLocation()
+    console.log(location)
+
     return (
         <header className="header">
             <div className="contenedor barra">
@@ -12,21 +17,25 @@ const Header = () => {
                     {/* Se usa link para navegar de una manera mas rapida que con "a" */}
                     <Link
                         to={"/"}
+                        className={location.pathname === "/" ? "active" : ""}
                     >
                         Inicio
                     </Link>
                     <Link
                         to={"/nosotros"}
+                        className={location.pathname === "/nosotros" ? "active" : ""}
                     >
                         Nosotros
                     </Link>
                     <Link
                         to={"/tienda"}
+                        className={location.pathname === "/tienda" ? "active" : ""}
                     >
                         Tienda
                     </Link>
                     <Link
                         to={"/blog"}
+                        className={location.pathname === "/blog" ? "active" : ""}
                     >
                         Blog
                     </Link>
