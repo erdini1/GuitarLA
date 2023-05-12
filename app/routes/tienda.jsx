7e61d45caecff2843bc2139e6f246de4fc7711c8
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-12 19:29:28 -0300

    Consumiendo el Backend de Strapi con un loader

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,3 +1,13 @@
+//Desde el momento en que exporto el loader ya se manda a llamar automatico. ya sabe lo que tiene que hacer
+// loader es lo que se utiliza cuando el componente carga
+// Utilizamos fetch api en el loader
+export async function loader(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras/?populate=imagen`)
+  const resultado = await respuesta.json()
+  console.log(resultado)
+  return {}
+}
+
 const Tienda = () => {
   return (
     <div>Tienda</div>
