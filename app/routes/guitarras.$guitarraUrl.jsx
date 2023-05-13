b45d0e16d64f20c3303ba15d9a8fc0c139eafa0f
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-13 19:39:13 -0300

    Mostrando la información de la guitarra

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,5 +1,15 @@
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
+import styles from "~/styles/guitarras.css"
+
+export function links(){
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ]
+}
 
 export async function loader({ params }) {
 
@@ -12,10 +22,18 @@ export async function loader({ params }) {
 const Guitarra = () => {
 
   const guitarra = useLoaderData()
-  console.log(guitarra.data[0].attributes.nombre)
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
 
   return (
-    <div>Guitarra URL</div>
+    <main className="contenedor guitarra">
+      <img className="imagen" src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
   )
 }
 
