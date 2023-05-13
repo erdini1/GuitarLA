3ce070d168884dc8d3e5d6539f1ff1cada2b4d2b
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-13 11:30:09 -0300

    Añadiendo un modelo que se ejecute unicamente en el servidor

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,11 +1,11 @@
+import { getGuitarras } from "~/models/guitarras.server"
+
 //Desde el momento en que exporto el loader ya se manda a llamar automatico. ya sabe lo que tiene que hacer
 // loader es lo que se utiliza cuando el componente carga
 // Utilizamos fetch api en el loader
 export async function loader(){
-  const respuesta = await fetch(`${process.env.API_URL}/guitarras/?populate=imagen`)
-  const resultado = await respuesta.json()
-  console.log(resultado)
-  return {}
+  const guitarras = await getGuitarras()
+  return guitarras    //Hacer commit y seguir con la siguiente parte
 }
 
 const Tienda = () => {
