cfed425c54cd209ff3a47b3f800f366bb9eb29f5
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-18 13:30:16 -0300

    Creando la ruta de carrito

--- app/components/navegacion.jsx
+++ app/components/navegacion.jsx
@@ -1,3 +1,4 @@
+import imagen from "../../public/img/carrito.png"
 import { Link, useLocation } from "@remix-run/react"
 const Navegacion = () => {
 
@@ -30,6 +31,12 @@ const Navegacion = () => {
                 className={location.pathname === "/blog" ? "active" : ""}
             >
                 Blog
+            </Link>
+
+            <Link
+                to={"/carrito"}
+            >
+                <img src={imagen} alt="carrito de compras" />
             </Link>
         </nav>
     )
