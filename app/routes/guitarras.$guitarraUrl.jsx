dce5b87fce4e13d42e3167e2e5c33d643810cce7
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-19 12:17:30 -0300

    Añadiendo un select en las guitarras

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -6,7 +6,7 @@ export async function loader({ params }) {
   const { guitarraUrl } = params
   const guitarra = await getGuitarra(guitarraUrl)
 
-  if(guitarra.data.length === 0){
+  if (guitarra.data.length === 0) {
     throw new Response("", {
       status: 404,
       statusText: "Guitarra No Encontrada"
@@ -17,10 +17,10 @@ export async function loader({ params }) {
 }
 
 //Una vez que loader pasa informacion este parametro de data va a estar disponible
-export function meta({data}){
+export function meta({ data }) {
   return [
-    {title: `GuitarLA - ${data.data[0].attributes.nombre}`},
-    {description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}`}
+    { title: `GuitarLA - ${data.data[0].attributes.nombre}` },
+    { description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}` }
   ]
 }
 
@@ -37,6 +37,25 @@ const Guitarra = () => {
         <h3>{nombre}</h3>
         <p className="texto">{descripcion}</p>
         <p className="precio">${precio}</p>
+
+        <form className="formulario">
+          <label htmlFor="">Cantidad</label>
+
+          <select name="" id="">
+            <option value="">-- Seleccione --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+
+          <input
+            type="submit"
+            value="Añadir al carrito"
+          />
+
+        </form>
       </div>
     </div>
   )
